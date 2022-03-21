e361be0ef713212f2e07c0baf6ba0378cbcbdae3
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-03-21 16:20:13 -0500

    refactored movie function and added movie rating. creaded movies.css file

--- movies.js
+++ movies.js
@@ -7,8 +7,10 @@ getMovies();
 function movieTitles(data){
     let html = ""
     for (let i = 0 ; i < data.length; i++){
-       var title = html += data[i].title
-        html += "<div>"+ title +"</div>";
+       var title = data[i].title
+       var rating = data[i].rating
+        html += "<div class='parent'>" + "<div class='title'>"+ title +"</div>"
+        +"<div class='raing'>"+ "Rating: " + rating +"</div>" + "</div>";
     }
     return html
 }

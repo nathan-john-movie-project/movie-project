617b8ef69133a7ee3e6feb30620e8209837114ed
John Castro <johhncaastro@gmail.com>
2022-03-21 15:38:42 -0500

    add jquery and bootstrap to html file and change up a few functions in js file

--- movies.js
+++ movies.js
@@ -1,5 +1,13 @@
 function getMovies(){
-    fetch("https://tropical-horn-mustard.glitch.me/movies").then(resp => resp.json()).then(data => console.log(data));
+    fetch("https://tropical-horn-mustard.glitch.me/movies").then(resp => resp.json()).then(data => $("#info").text(movieTitles(data)));
 }
 
 getMovies();
+
+function movieTitles(data){
+    let titles = ""
+    for (let i = 0 ; i < data.length; i++){
+        data[i].title += titles
+    }
+    return "<h1>"+ titles +"</h1>"
+}
